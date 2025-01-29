adc524f19becf4c22953f39837271317cb9b32de
wangzhizhou <wangzhizhou@bytedance.com>
2025-01-29 21:15:34 +0800

    commit

--- OrzNFCReader/App/Model/OrzNFCError.swift
+++ OrzNFCReader/App/Model/OrzNFCError.swift
@@ -1,5 +1,7 @@
 @preconcurrency import CryptoTokenKit
 
 enum OrzNFCError: String, Error {
-    case invalidSmartCardManager = "The com.apple.security.smartcard entitlement is required in order to use TKSmartCardSlotManager."
+    case invalidSmartCardManager = """
+    The com.apple.security.smartcard entitlement is required in order to use TKSmartCardSlotManager.
+    """
 }
